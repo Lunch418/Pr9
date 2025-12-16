b773989a5b61025fe0064d55fed5f2f875708e9f
Артем Сыпачев <Lunch418@bk.ru>
2025-12-16 20:58:03 +0500

    Добавьте файлы проекта.

---
+++ TaskManagerTelegramBot/Classes/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot.Classes
+{
+     public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser, List<Events> events)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
